c3e13f58ccaa0ed1f23076486263aa1c60988966
RikiLucy <from.riki.with.love@gmail.com>
2016-09-26 22:27:43 +0700

    Модель для комментариев

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['author', 'text'];
+    //protected $fillable = ['author', 'text', 'date'];
     static public function getComments($id){
         $comments = Comment::where('id_article','=', $id)->get();
         return $comments;
