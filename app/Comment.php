5b3e1ae475871f4544c989692c84accf5f596935
RikiLucy <riki.and.lucy@gmail.com>
2016-09-27 20:22:54 +0700

    Модель для комментариев

--- app/Comment.php
+++ app/Comment.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
+class Comment extends Model //модель для комментариев
 {
     //protected $fillable = ['author', 'text', 'date'];
     static public function getComments($id){
