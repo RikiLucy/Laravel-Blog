31c97fd88f79b888b65e172576f5133dd46dffcf
RikiLucy <riki.and.lucy@gmail.com>
2016-10-03 16:59:22 +0300

    Удалил лишнюю функцию

--- app/Comment.php
+++ app/Comment.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model //модель для комментариев
 {
-    //protected $fillable = ['author', 'text', 'date'];
-    static public function getComments($id){
-        $comments = Comment::where('id_article','=', $id)->get();
-        return $comments;
-    }
+    
 }
