6c26ba772b3c6f13e40e1f10abc0de23d2fba5c4
RikiLucy <riki.and.lucy@gmail.com>
2016-10-06 00:24:48 +0700

    Пересоздал миграции

--- database/migrations/2016_10_05_152245_create_categories_table.php
+++ database/migrations/2016_10_05_152245_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('article_id')->nullable();
-            $table->string('categorie_id')->nullable();
+            $table->string('name')->nullable();
+            $table->integer('count')->nullable();
             $table->timestamps();
         });
     }
