c4688de4582c026c0bc7d42f28fb1fe0ab409795
RikiLucy <riki.and.lucy@gmail.com>
2016-10-07 22:51:10 +0700

    Исправил таблицу с категориями

--- database/migrations/2016_10_05_152245_create_categories_table.php
+++ database/migrations/2016_10_05_152245_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable();
+            $table->string('categories')->nullable();
             $table->integer('count')->nullable();
             $table->timestamps();
         });
